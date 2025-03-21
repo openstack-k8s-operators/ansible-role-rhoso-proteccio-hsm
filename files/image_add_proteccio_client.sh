52184e6907dde5a52ba1cd2c03e0879bb616c68e
Mauricio Harley <mharley@redhat.com>
2025-03-21 18:12:34 +0000

    Ansible role for customizing Barbican API and Worker images to support Eviden Proteccio HSMs.
    Refers to: OSPRH-14696.
    
    Signed-off-by: Mauricio Harley <mharley@redhat.com>

---
+++ files/image_add_proteccio_client.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# barbican_add_proteccio_client.sh
+#
+# This script adds the Linux Minimal Client for Eviden Trustway Proteccio HSM
+# to both the API and Worker images so that the HSM can be used as a PKCS#11
+# backend for Barbican.
+set -x
+set -o errexit
+set -o pipefail
+
+BARBICAN_SRC_IMAGE_REGISTRY=${BARBICAN_SRC_IMAGE_REGISTRY:-"quay.io"}
+BARBICAN_SRC_IMAGE_NAMESPACE=${BARBICAN_SRC_IMAGE_NAMESPACE:-"podified-antelope-centos9"}
+BARBICAN_SRC_IMAGE_TAG=${BARBICAN_SRC_IMAGE_TAG:-"current-podified"}
+BARBICAN_SRC_API_IMAGE="$BARBICAN_SRC_IMAGE_REGISTRY/$BARBICAN_SRC_IMAGE_NAMESPACE/openstack-barbican-api:$BARBICAN_SRC_IMAGE_TAG"
+BARBICAN_SRC_WORKER_IMAGE="$BARBICAN_SRC_IMAGE_REGISTRY/$BARBICAN_SRC_IMAGE_NAMESPACE/openstack-barbican-worker:$BARBICAN_SRC_IMAGE_TAG"
+
+BARBICAN_DEST_IMAGE_REGISTRY=${BARBICAN_DEST_IMAGE_REGISTRY:-"quay.io"}
+BARBICAN_DEST_IMAGE_NAMESPACE=${BARBICAN_DEST_IMAGE_NAMESPACE:-"rh_ee_mharley"}
+BARBICAN_DEST_IMAGE_TAG=${BARBICAN_DEST_IMAGE_TAG:-"current-podified"}
+BARBICAN_DEST_API_IMAGE="$BARBICAN_DEST_IMAGE_REGISTRY/$BARBICAN_DEST_IMAGE_NAMESPACE/openstack-barbican-api:$BARBICAN_DEST_IMAGE_TAG"
+BARBICAN_DEST_WORKER_IMAGE="$BARBICAN_DEST_IMAGE_REGISTRY/$BARBICAN_DEST_IMAGE_NAMESPACE/openstack-barbican-worker:$BARBICAN_DEST_IMAGE_TAG"
+
+# PROTECCIO_LINUX_CLIENT_DIR - location of the linux client directory
+# in your client media.  This could be a path to a mounted ISO or a path to
+# the location where a tarball was extracted.
+PROTECCIO_LINUX_CLIENT_DIR=${PROTECCIO_LINUX_CLIENT_DIR:-"/mnt/proteccio_iso"}
+
+VERIFY_TLS=${VERIFY_TLS:-"true"}
+
+function install_client() {
+
+  if [ "$VERIFY_TLS" == "true" ]; then
+    container=$(buildah from $1)
+  else
+    container=$(buildah from --tls-verify=false $1)
+  fi
+
+  # set required env
+  buildah config --env ConfigurationPath=/etc/proteccio $container
+
+  # add Linux client
+  buildah run --user root $container -- mkdir -p /tmp/proteccio
+  buildah add --chown root:root $container $PROTECCIO_LINUX_CLIENT_DIR /tmp/proteccio
+  buildah run --user root $container -- cd /tmp/proteccio/Linux
+  buildah run --user root $container -- bash -c "cd /tmp/proteccio/Linux; { echo \"e\"; echo \"n\"; echo; } | ./install.sh"
+
+  if [ "$VERIFY_TLS" == "true" ]; then
+    buildah commit $container $2
+    podman push $2
+  else
+    buildah commit --tls-verify=false $container $2
+    podman push --tls-verify=false $2
+  fi
+  buildah rm $container
+}
+
+install_client $BARBICAN_SRC_API_IMAGE $BARBICAN_DEST_API_IMAGE
+install_client $BARBICAN_SRC_WORKER_IMAGE $BARBICAN_DEST_WORKER_IMAGE
